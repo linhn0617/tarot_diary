7cd20794fc958ed8382f0a8a896fc580dfbb02b7
linhn0617 <yyo833782@gmail.com>
2025-02-23 01:32:36 +0800

    feat: 在api.php 中新增 void 的 return type

--- routes/api.php
+++ routes/api.php
@@ -25,12 +25,12 @@ Route::prefix('auth')->group(function (): void {
 Route::get('/tarot/draw', [TarotDrawController::class, 'drawCard']);
 
 // 註冊、驗證使用者信箱、登入、登出、刷新 token
-Route::prefix('auth')->group(function () {
+Route::prefix('auth')->group(function (): void {
     Route::post('/register', [AuthController::class, 'register'])->middleware('throttle:6,1');
     Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:6,1');
     Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verifyEmail'])->name('verification.verify');
 
-    Route::middleware('auth:api')->group(function () {
+    Route::middleware('auth:api')->group(function (): void {
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::post('/refresh', [AuthController::class, 'refresh']);
     });
