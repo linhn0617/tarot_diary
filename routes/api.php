a103fecde0b63bab099f6c2c87524944c5092342
linhn0617 <yyo833782@gmail.com>
2025-02-23 01:27:08 +0800

    feat: 新增註冊、驗證使用者信箱、登入、登出、刷新 token 的route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// 註冊、驗證使用者信箱、登入、登出、刷新 token
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register'])->middleware('throttle:6,1');
+    Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:6,1');
+    Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verifyEmail'])->name('verification.verify');
+
+    Route::middleware('auth:api')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::post('/refresh', [AuthController::class, 'refresh']);
+    });
+});
