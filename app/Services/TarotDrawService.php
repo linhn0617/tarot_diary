e561410c8276426c1658532b64b07335e38e9057
fufuYang <imfuting@gmail.com>
2025-03-04 22:04:35 +0800

    refactor: 在 TarotSpecification Model 綁定 Tarot 關聯，移除不必要的查詢

--- app/Services/TarotDrawService.php
+++ app/Services/TarotDrawService.php
@@ -6,25 +6,28 @@ use App\Models\Tarot;
 use App\Models\TarotSpecification;
 
 /**
- * Service class for drawing tarot cards
+ * Service class for drawing tarot cards.服務類別：塔羅牌抽取
+ *
+ * 此類別負責從 `tarot_specifications` 表中隨機抽取一張塔羅牌，並返回其相關資訊。
  */
 class TarotDrawService
 {
     /**
+     * 隨機抽取一張塔羅牌，並返回牌的詳細資訊
      * @return array{tarot_id: int, image: string, name: string, is_upright: bool, message: string}
      */
     public function drawCard(): array
     {
         // 隨機抽取一張塔羅牌
-        $tarotSpecification = TarotSpecification::inRandomOrder()->firstOrFail();
-        /** @var Tarot $tarot */
-        $tarot = Tarot::findOrFail($tarotSpecification->tarot_id);
+        $tarotSpecification = TarotSpecification::with('tarot')->inRandomOrder()->firstOrFail();
+
+        // 隨機選擇一個關心小語
         $message = rand(0, 1) ? $tarotSpecification->message1 : $tarotSpecification->message2;
 
         return [
-            'tarot_id' => $tarot->id,
-            'image' => $tarot->image_path,
-            'name' => $tarot->name,
+            'tarot_id' => $tarotSpecification->tarot->id,
+            'image' => $tarotSpecification->tarot->image_path,
+            'name' => $tarotSpecification->tarot->name,
             'is_upright' => $tarotSpecification->is_upright,
             'message' => $message,
         ];
