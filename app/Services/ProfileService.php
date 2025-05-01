a9e97e68643640b6650b91ed0d3ab130f5ec27e8
linhn0617 <yyo833782@gmail.com>
2025-05-01 10:12:54 +0800

    feat(profile): add user profile completion for social login

---
+++ app/Services/ProfileService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+
+class ProfileService
+{
+    /**
+     * 更新使用者個人資料（名稱、性別、生日）
+     */
+    public function updateProfile(User $user, array $data): void
+    {
+        $user->update([
+            'name' => $data['name'],
+            'gender' => $data['gender'],
+            'birthday' => $data['birthday'],
+            'profile_completed' => true,
+        ]);
+    }
+}
